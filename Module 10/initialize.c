7b2aa0254f161099dac0b0b2419fa8b2dc2f5643
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-10-16 20:21:30 -0700

    string power

---
+++ Module 10/initialize.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    // power  of string
+    // Initialization
+    // char a[5] = {'M', 'u' , 'k' , 't' , 'a'};
+    // char a[5] = "Mukta";
+    // for(int i = 0; i<5; i++){
+    //     printf("%c " , a[i]);
+    // }
+    // Print
+    
+    return 0;
+}
