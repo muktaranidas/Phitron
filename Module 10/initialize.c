609790eed456b077eee39c74444f29ca7707dc15
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-10-16 20:24:40 -0700

    done string

--- Module 10/initialize.c
+++ Module 10/initialize.c
@@ -2,13 +2,21 @@
 int main ()
 {
     // power  of string
+
+
     // Initialization
     // char a[5] = {'M', 'u' , 'k' , 't' , 'a'};
     // char a[5] = "Mukta";
     // for(int i = 0; i<5; i++){
     //     printf("%c " , a[i]);
     // }
+
+
     // Print
+     // char a[5] = {'M', 'u' , 'k' , 't' , 'a'};
+    char a[6] = "Mukta\0";
+    printf("%s " , a);
+    
     
     return 0;
 }
