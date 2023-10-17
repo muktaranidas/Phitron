e028e4f182ac566b15d90eafbaa7e7abca3bcf48
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-10-17 16:54:14 -0700

    done i love string

--- Week 3 Conceptual session 1/index.c
+++ Week 3 Conceptual session 1/index.c
@@ -2,10 +2,25 @@
 #include<string.h>
 int main ()
 {
-    char ch[5];
+    // char ch[5];
+    // // scanf("%s" , ch);
+    // // gets(ch);
+    // fgets(ch, 5, stdin);
+    // printf("%s %d %d\n" , ch, strlen(ch), sizeof(ch));
+
+    int t;
+    scanf("%d" , &t);
+    
+    getchar();
+    while (t--)
+    {
+    char ch[100];
     // scanf("%s" , ch);
-    // gets(ch);
-    fgets(ch, 5, stdin);
-    printf("%s %d %d\n" , ch, strlen(ch), sizeof(ch));
+    //   gets(ch);
+       fgets(ch, 100, stdin);
+    printf("%s\n" , ch);
+      
+    } 
+    
     return 0;
 }
