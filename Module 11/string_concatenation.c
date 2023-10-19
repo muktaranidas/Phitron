9e0e92a7008b52627c5b255e8a8c62d90da54fa2
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-10-18 23:52:05 -0700

    String Concatenation

--- Module 11/string_concatenation.c
+++ Module 11/string_concatenation.c
@@ -4,11 +4,11 @@ int main ()
 {
     char a[200] , b[100];
     scanf("%s %s" , a,b);
-    // int k = strlen(a);
-    // for(int i=0; i<strlen(b) ; i++){
-    //     a[k] = b[i];
-    //     k++;
-    // }
+    int k = strlen(a);
+    for(int i=0; i<strlen(b) ; i++){
+        a[k] = b[i];
+        k++;
+    }
     printf("%s %s" , a,b);
     return 0;
 }
