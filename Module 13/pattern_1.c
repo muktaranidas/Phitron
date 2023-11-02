6a246c3e01503bd3bebaa6d6825b7419d63d8769
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-02 14:45:50 -0700

    examplete 2

--- Module 13/pattern_1.c
+++ Module 13/pattern_1.c
@@ -1,7 +1,25 @@
+// #include<stdio.h>
+// int main ()
+// {
+//     int n,k=1;
+//     scanf("%d" , &n);
+//     for(int i=1; i<=n; i++){  //line
+//          //kaj  start
+//        for(int j=1; j<=k; j++){
+//         printf("*");
+//        }
+//         //kaj end   
+//         k++;
+//         printf("\n");
+  
+//     }
+//     return 0;
+// }
+
 #include<stdio.h>
 int main ()
 {
-    int n,k=1;
+    int n,k=4;
     scanf("%d" , &n);
     for(int i=1; i<=n; i++){  //line
          //kaj  start
@@ -9,7 +27,7 @@ int main ()
         printf("*");
        }
         //kaj end   
-        k++;
+        k--;
         printf("\n");
   
     }
