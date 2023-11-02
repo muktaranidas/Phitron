95bc1e0aec03a37ebcf6aefefca50154858165af
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-02 15:02:43 -0700

    example 2 piramid pattern nested loop

---
+++ Module 13/pattern_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    int n,s,k;
+    scanf("%d" , &n);
+    s=n-1;
+    k=1;
+    for(int i=1; i<=n; i++){
+        //Line start
+        for(int j=1; j<=s; j++){
+            printf(" ");
+        }
+        for(int j=1; j<=k; j++){
+            printf("*");
+        }
+        //Line end
+        s--;
+        k=k+2;
+        printf("\n");
+    }
+    return 0;
+}
