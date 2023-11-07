1dd76251e701cec68497659edccad3edc361c0b4
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-07 15:49:41 -0800

    complete build in function

--- Module 14/No-return+No-Parameter.c
+++ Module 14/No-return+No-Parameter.c
@@ -3,10 +3,12 @@ void sum(void){
     int a,b;
     scanf("%d %d" , &a,&b);
     int s=a+b;
-    printf("%d" , s);
+    printf("%d\n" , s);
 }
 int main ()
 {
     sum();
+    sum();
+
     return 0;
 }
