8cd523584b035d54c02bec5dddc19c960d04463a
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-10 17:03:01 -0800

    difference array

--- Module 20/Difference_Array.c
+++ Module 20/Difference_Array.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main() {
-    int T; // Number of test cases
-    scanf("%d", &T);
-
-    while (T--) {
-        int N; // Size of the array
-        scanf("%d", &N);
-
-        int A[1000], B[1000], C[1000];
-
-        // Input array A
-        for (int i = 0; i < N; i++) {
-            scanf("%d", &A[i]);
-            B[i] = A[i]; // Make a copy of array A
+    int t; 
+    scanf("%d", &t);
+    while (t--) {
+        int n; 
+        scanf("%d", &n);
+        int a[1000], b[1000], c[1000];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+            b[i] = a[i]; 
         }
-
-        // Sort array B in ascending order
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - i - 1; j++) {
-                if (B[j] > B[j + 1]) {
-                    // Swap elements if they are in the wrong order
-                    int temp = B[j];
-                    B[j] = B[j + 1];
-                    B[j + 1] = temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (b[j] > b[j + 1]) {
+                    int temp = b[j];
+                    b[j] = b[j + 1];
+                    b[j + 1] = temp;
                 }
             }
         }
-
-        // Calculate array C
-        for (int i = 0; i < N; i++) {
-            C[i] = abs(A[i] - B[i]);
+        for (int i = 0; i < n; i++) {
+            c[i] = abs(a[i] - b[i]);
         }
-
-        // Output array C
-        for (int i = 0; i < N; i++) {
-            printf("%d", C[i]);
-            if (i < N - 1) {
+        for (int i = 0; i < n; i++) {
+            printf("%d", c[i]);
+            if (i < n - 1) {
                 printf(" ");
             }
         }
