fee382ee71ec622cd412c55b742fef7afac53eb9
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-11-10 16:44:10 -0800

    Tiger vs Pathan

--- Module 20/Tiger_vs_Pathan.c
+++ Module 20/Tiger_vs_Pathan.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 int main() {
-    int T; // Number of test cases
-    scanf("%d", &T);
-    while (T--) {
-        int N; // Number of rounds
-        scanf("%d", &N);
-        char S[N+1]; // String of size N
-        scanf("%s", S);
-        // Count the number of 'T' and 'P' in the string
-        int count_T = 0, count_P = 0;
-        for (int i = 0; i < N; i++) {
-            if (S[i] == 'T') {
-                count_T++;
-            } else if (S[i] == 'P') {
-                count_P++;
+    int t; 
+    scanf("%d", &t);
+    while (t--) {
+        int n; 
+        scanf("%d", &n);
+        char size[n+1]; 
+        scanf("%s", size);
+        int countForTiger = 0, countForPathaan = 0;
+        for (int i = 0; i < n; i++) {
+            if (size[i] == 'T') {
+                countForTiger++;
+            } else if (size[i] == 'P') {
+                countForPathaan++;
             }
         }
-        // Compare the counts and determine the winner
-        if (count_T > count_P) {
+        if (countForTiger > countForPathaan) {
             printf("Tiger\n");
-        } else if (count_P > count_T) {
+        } else if (countForPathaan > countForTiger) {
             printf("Pathaan\n");
         } else {
             printf("Draw\n");
